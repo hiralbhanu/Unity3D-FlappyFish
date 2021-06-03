46e53ab3ad81c57a7b61f669bd764e1ed6d3534f
hiralbhanu <hiralbhanushali13@gmail.com>
2021-06-03 22:55:33 +0530

    Add files via upload

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 1f;
+    [SerializeField]
+    private float randomOffset = 2f;
+
+    private void Update()
+    {
+        transform.position += Time.deltaTime * speed * Vector3.left;
+        if (transform.position.x <= -15)
+        {
+            float randomHeight = UnityEngine.Random.Range(-randomOffset, randomOffset); 
+            transform.position = new Vector3(15, randomHeight, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/seaweed.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class seaweed : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<Fish>() != null)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
